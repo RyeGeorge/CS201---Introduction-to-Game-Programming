b5d457d4e4e055310451f6b2f4e363e15c3a7042
RyeG <ryejackg@gmail.com>
2023-04-11 17:18:52 +1200

    Worked on enemy AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -16,14 +17,23 @@ public class EnemyAI : MonoBehaviour
     private bool facingRight = false;
 
     [Header("Attack")]
+    public float damage;
     public float attackRange;
     public float attackDelay;
     private bool attacking;
 
     [Header("Health")]
     public float health;
-    private float currentHealth;
+    private float currentHealth; 
+    public Vector2 deathKnockback;
+    private bool dead = false;
 
+
+    [Header("Healthbar")]
+    public Canvas canvas;
+    public Slider healthbar;
+    public float deactivateTime;
+    private bool healthBarActive;
 
     private bool attackRunning, moveRightRunning, moveLeftRunning;
 
@@ -40,48 +50,51 @@ public class EnemyAI : MonoBehaviour
 
     private void Update()
     {
-        if (Vector2.Distance(this.transform.position, player.transform.position) <= attackRange && !attackRunning)
+        if (!dead)
         {
-            StopAllCoroutines();
-            attacking = true;
+            if (Vector2.Distance(this.transform.position, player.transform.position) <= attackRange && !attackRunning)
+            {
+                StopAllCoroutines();
+                attacking = true;
 
-            StartCoroutine(Attack());
-            
-        }
-        else if (Vector2.Distance(this.transform.position, player.transform.position) > attackRange && attackRunning)
-        {
-            attacking = false;
-            attackRunning = false;
-            moveRightRunning = false;
-            moveLeftRunning = false;
-            StopCoroutine(Attack());
+                StartCoroutine(Attack());
 
-            if (movingRight && moveRightRunning == false)
-                StartCoroutine(MoveRight());
-            else if (!movingRight && moveLeftRunning == false)
-                StartCoroutine(MoveLeft());
-        }
+            }
+            else if (Vector2.Distance(this.transform.position, player.transform.position) > attackRange && attackRunning)
+            {
+                attacking = false;
+                attackRunning = false;
+                moveRightRunning = false;
+                moveLeftRunning = false;
+                StopCoroutine(Attack());
+
+                if (movingRight && moveRightRunning == false)
+                    StartCoroutine(MoveRight());
+                else if (!movingRight && moveLeftRunning == false)
+                    StartCoroutine(MoveLeft());
+            }
 
 
-        if (movingRight && !facingRight)
-            Flip();
-
-        else if (!movingRight && facingRight)
-            Flip();
-
-        if (attacking)
-        {
-            if (facingRight && transform.position.x > player.transform.position.x)
+            if (movingRight && !facingRight)
                 Flip();
 
-            else if (!facingRight && transform.position.x < player.transform.position.x)
+            else if (!movingRight && facingRight)
                 Flip();
-        }
 
-        if (moveRightRunning || moveLeftRunning)
-            anim.SetBool("Moving", true);
-        else
-            anim.SetBool("Moving", false);
+            if (attacking)
+            {
+                if (facingRight && transform.position.x > player.transform.position.x)
+                    Flip();
+
+                else if (!facingRight && transform.position.x < player.transform.position.x)
+                    Flip();
+            }
+
+            if (moveRightRunning || moveLeftRunning)
+                anim.SetBool("Moving", true);
+            else
+                anim.SetBool("Moving", false);
+        }
     }
 
     private void Flip()
@@ -147,22 +160,77 @@ public class EnemyAI : MonoBehaviour
                 TakeDamage(playerBulletProjectile.damage);
             }
         }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DamagePlayer(collision.gameObject.GetComponentInParent<PlayerHealth>());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (dead && collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            anim.SetTrigger("Dead Landed");
+        }
+    }
+
+    void DamagePlayer(PlayerHealth playerHealth)
+    {
+        playerHealth.DecreaseHealth(damage);
     }
 
     void TakeDamage(float damage)
     {
-        currentHealth -= damage;
-
-        if (currentHealth <= 0)
+        if (!dead)
         {
-            currentHealth = 0;
+            if (!healthBarActive)
+            {
+                ActivateHealthBar();
+            }
 
-            KillEnemy();
+            currentHealth -= damage;
+            anim.SetTrigger("Hit");
+
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+
+                KillEnemy();
+            }
         }
+    }
+
+    void ActivateHealthBar()
+    {
+        healthbar.enabled = true;
+        healthbar.transform.SetParent(canvas.transform);
+
+        CancelInvoke();
+        Invoke("DeactivateHealthBar", deactivateTime);
+    }
+
+    void DeactivateHealthBar()
+    {
+        healthbar.enabled = false;
     }
 
     void KillEnemy()
     {
-        Debug.Log("Dead");
+        dead = true;
+        anim.SetTrigger("Dead");
+        StopAllCoroutines();
+        ApplyDeathKnockback();
+    }
+
+    void ApplyDeathKnockback()
+    {
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.velocity = Vector3.zero;
+
+        if (facingRight)
+            rb.velocity = new Vector2(-1 * deathKnockback.x, deathKnockback.y);
+        else
+            rb.velocity = new Vector2(1 * deathKnockback.x, deathKnockback.y);
     }
 }
